84744de2e451f635954a0df26924783473881304
Adam Cooper <adam.cooper@gmail.com>
2012-04-06 10:55:45 -0600

    middleman attempt..

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source :rubygems
 
-gem 'jekyll'
-gem 'compass'
-gem 'pygments'
+gem "middleman", "3.0.0.beta.2", :git => 'https://github.com/middleman/middleman.git'
+gem "middleman-blog", "0.2.0.pre", :git => 'https://github.com/middleman/middleman-blog.git'
+gem "sass"
--- config.rb
+++ config.rb
@@ -1,9 +1,106 @@
-# Require any additional compass plugins here.
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+###
+## Blog settings
+####
+
+activate :blog
+
+# set :blog_permalink, ":year/:month/:day/:title.html"
+# set :blog_sources, ":year-:month-:day-:title.html"
+# set :blog_taglink, "tags/:tag.html"
+# set :blog_layout, "layout"
+# set :blog_summary_separator, /(READMORE)/
+# set :blog_summary_length, 250
+# set :blog_year_link, ":year.html"
+# set :blog_month_link, ":year/:month.html"
+# set :blog_day_link, ":year/:month/:day.html"
+# set :blog_default_extension, ".markdown"
+
+set :blog_tag_template, "tag.html"
+set :blog_calendar_template, "calendar.html"
+
+page "/feed.xml", :layout => false
+
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install compass-susy-plugin
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Change the CSS directory
+# set :css_dir, "alternative_css_directory"
+
+# Change the JS directory
+# set :js_dir, "alternative_js_directory"
+
+# Change the images directory
+# set :images_dir, "alternative_image_directory"
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
